12e2a526086432fbb04797f77b3ebd20ea1517a7
Administrator <administrator@ubuntu.(none)>
2013-07-08 05:24:07 -0400

    Initial commit

---
+++ app/controllers/entry_controller.rb
@@ -0,0 +1,16 @@
+class EntryController < ApplicationController
+  def home
+  end
+
+  def portfolio
+  end
+
+  def services
+  end
+
+  def faq
+  end
+
+  def contact
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DeveApp::Application.config.secret_key_base = 'f97ed3e17be43f181901bdc2709787d64497ebad4710e1df2aca8e5fa97b5e71a3b784fc9f9977e9099ecc6ee89244fc727d372d4603dd093604b16c2ff1dda3'
