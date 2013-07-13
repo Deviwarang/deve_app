ae5905cedf085cdaef0770b7593cefb91e2c7040
Administrator <administrator@ubuntu.(none)>
2013-07-13 06:52:54 -0400

    changes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 DeveApp::Application.routes.draw do
-  root  "entry#home"
+  root  to: 'entry#home'
 
-  get "/home", to: "entry#home"
-  get "/portfolio", to: "entry#portfolio"
-  get "/why", to:  "whyreq#why"
-  get  "/services", to: "entry#services"
-  get  "/faq", to: "entry#faq"
-  get "/contact", to: "entry#contact"
+  get '/home', to: 'entry#home'
+  get '/portfolio', to: 'entry#portfolio'
+  get '/why', to:  'whyreq#why'
+  get  '/services', to: 'entry#services'
+  get  '/faq', to: 'entry#faq'
+  get '/contact', to: 'entry#contact'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
